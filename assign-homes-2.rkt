0c9bb16778381bf500348ebad27b2612dbd6e8e1
RyanGlScott <ryan.gl.scott@gmail.com>
2016-01-25 18:38:05 -0500

    Interference graph

--- assign-homes-2.rkt
+++ assign-homes-2.rkt
@@ -1,5 +1,7 @@
 #lang racket
 
+(require "utilities.rkt")
+
 (provide assign-homes)
 
 (define (varToStackPos expression listHomes)
@@ -25,12 +27,18 @@
 ; x86* -> x86*
 (define uncover-live
   (match-lambda
-    [`(program ,vars ,instrs ...)
+    [`(program (,vars ...) ,instrs ...)
      (let
-         ([live-afters (foldr uncover-live-help
-                              `(,(set))
-                              instrs)])
-       `(program (,vars ,live-afters) ,@instrs))]))
+         ([live-afters
+           (drop (foldr
+                  uncover-live-help
+                  `(,(set))
+                  ; ^ We're a bit dishonest here in that we're matching up instruction
+                  ; k with live-after set k-1 (since this empty set we pass is
+                  ; live-after set k). But that's okay, we'll just end up creating one
+                  ; extra live-after set which is guaranteed to be empty, so we drop it.
+                  instrs) 1)])
+       `(program (,@vars ,live-afters) ,@instrs))]))
 
 ; Instruction -> Set Variable -> Set Variable
 (define uncover-live-help
@@ -38,6 +46,7 @@
     (match live-after-set
       [(list-rest live-after-k+1 live-after-rest)
        (let* ([live-before-k+1
+               ; L_before(k+1) = (L_after(k+1) - W(k+1)) U R(k + 1)
                (set-union (set-subtract live-after-k+1
                                         (written-variables instr-k+1))
                           (read-variables instr-k+1))]
@@ -64,3 +73,58 @@
   (match-lambda
     [`(,(or `var `reg) ,name) (set name)]
     [`(int ,_)                (set)]))
+
+; Arg -> (Int | Symbol) [not-particularly well-typed]
+(define arg-payload
+  (match-lambda
+    [`(,_ ,payload) payload]))
+
+; x86* -> x86*
+(define build-interference
+  (match-lambda
+    [`(program (,vars ... ,live-afters) ,instrs ...)
+     (let* ([graph (foldl (curry add-edge-interference)
+                          (make-graph vars)
+                          instrs
+                          live-afters)])
+       `(program (,@vars ,graph) ,@instrs))]))
+
+; Instruction -> Set Variable -> Graph -> Graph
+(define add-edge-interference
+  (lambda (instr live-after-set graph)
+    (match instr
+      [`(movq ,s ,d)
+       (let [(s-pl (arg-payload s))
+             (d-pl (arg-payload d))]
+         (sequence-fold
+          (λ (gr v)
+            (if (or (eqv? v s-pl) (eqv? v d-pl))
+                gr
+                (begin
+                  (add-edge gr d-pl v)
+                  gr)))
+          graph
+          live-after-set))]
+      [`(,(or `addq `subq) ,_ ,d)
+       (let [(d-pl (arg-payload d))]
+         (sequence-fold
+          (λ (gr v)
+            (if (eqv? v d-pl)
+                gr
+                (begin
+                  (add-edge gr d-pl v)
+                  gr)))
+          graph
+          live-after-set))]
+      [`(callq ,label)
+       (sequence-fold
+        (λ (gr v)
+          (sequence-fold
+           (λ (gr^ r)
+             (begin
+               (add-edge gr^ r v)
+               gr^))
+           gr
+           callee-save))
+        graph
+        live-after-set)])))
