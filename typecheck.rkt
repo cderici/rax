5a1d7ba2ab2b5b0c467f91fd86562bdc5753c831
RyanGlScott <ryan.gl.scott@gmail.com>
2016-03-13 20:35:05 -0400

    Add has-types to typechecker

--- typecheck.rkt
+++ typecheck.rkt
@@ -9,48 +9,56 @@
   (λ (env)
     (λ (expr)
       (match expr
-        [(? fixnum?)  `Integer]
-        [(? boolean?) `Boolean]
+        [(? fixnum?)
+         `(has-type ,expr Integer)]
+        [(? boolean?)
+         `(has-type ,expr Boolean)]
         [`(vector ,exp1 ,exps ...)
-         (let ([ts (map (typecheck env) (cons exp1 exps))])
-           `(Vector ,@ts))]
-        [(? symbol?)  (lookup expr env)]
-        [`(void)      `Void]
-        [`(read)      `Integer]
-        [`(lambda: ([,xs : ,ty-args] ...) : ,ty-ret ,body)
-         (let* ([new-env (append (map cons xs ty-args) env)]
-                [ty-body ((typecheck new-env) body)])
+         (match-let
+             ([(and es (list `(has-type ,_ ,ts) ...))
+               (map (typecheck env) (cons exp1 exps))])
+           `(has-type (vector ,@es) (Vector ,@ts)))]
+        [(? symbol?)
+         `(has-type ,expr ,(lookup expr env))]
+        [`(void)
+         `(has-type ,expr Void)]
+        [`(read)
+         `(has-type ,expr Integer)]
+        [`(lambda: ,(and args `([,xs : ,ty-args] ...)) : ,ty-ret ,body)
+         (match-let* ([new-env (append (map cons xs ty-args) env)]
+                      [(and body^ `(has-type ,_ ,ty-body)) ((typecheck new-env) body)])
            (if (equal? ty-ret ty-body)
-               `(,@ty-args -> ,ty-ret)
+               `(has-type (lambda: ,args : ,ty-ret ,body^) `(,@ty-args -> ,ty-ret))
                (type-error ty-ret ty-body body expr)))]
         [`(let ([,x ,e]) ,body)
-         (let* ([t ((typecheck env) e)]
-                [new-env (cons (cons x t) env)])
-           ((typecheck new-env) body))]
+         (match-let* ([(and e^ `(has-type ,_ ,t)) ((typecheck env) e)]
+                      [new-env (cons (cons x t) env)]
+                      [(and body^ `(has-type ,_ ,t-body)) ((typecheck new-env) body)])
+           `(has-type (let ([,x ,e^]) ,body^) ,t-body))]
         [`(- ,e)
-         (tc-unary-expr `Integer `Integer e env expr)]
+         (tc-unary-expr  `-   `Integer `Integer e env expr)]
         [`(+ ,e1 ,e2)
-         (tc-binary-expr `Integer `Integer e1 e2 env expr)]
+         (tc-binary-expr `+   `Integer `Integer e1 e2 env expr)]
         [`(not ,e)
-         (tc-unary-expr `Boolean `Boolean e env expr)]
+         (tc-unary-expr  `not `Boolean `Boolean e env expr)]
         [`(and ,e1 ,e2)
-         (tc-binary-expr `Boolean `Boolean e1 e2 env expr)]
+         (tc-binary-expr `and `Boolean `Boolean e1 e2 env expr)]
         [`(if ,c ,t ,f)
          (match ((typecheck env) c)
-           [`Boolean
-            (let ([t-ty ((typecheck env) t)]
-                  [f-ty ((typecheck env) f)])
+           [(and c^ `(has-type ,_ Boolean))
+            (match-let ([(and t^ `(has-type ,_ ,t-ty)) ((typecheck env) t)]
+                        [(and f^ `(has-type ,_ ,f-ty)) ((typecheck env) f)])
               (if (equal? t-ty f-ty)
-                  t-ty
+                  `(has-type (if ,c^ ,t^ ,f^) ,t-ty)
                   (type-error t-ty f-ty f expr)))]
            [actual-ty (type-error `Boolean actual-ty c expr)])]
         [`(eq? ,e1 ,e2)
          ; Bit of an odd case, since it can take
          ; two booleans or two fixnums
-         (let ([e1-ty ((typecheck env) e1)]
-               [e2-ty ((typecheck env) e2)])
+         (match-let ([(and e1^ `(has-type ,_ ,e1-ty)) ((typecheck env) e1)]
+                     [(and e2^ `(has-type ,_ ,e2-ty)) ((typecheck env) e2)])
            (if (equal? e1-ty e2-ty)
-               `Boolean
+               `(has-type (eq? ,e1^ ,e2^) Boolean)
                (error (format (unlines `("Type error:"
                                          "\tExpected:\ttwo equal types"
                                          "\tActual:\t\t~a"
@@ -61,46 +69,59 @@
                               e1-ty e2-ty e1 e2 expr))))]
         [`(vector-ref ,vec-exp ,ix)
          (match ((typecheck env) ix)
-           [`Integer
-            (match ((typecheck env) vec-exp)
-              [`(Vector ,ty1 ,tys ...)
-               (list-ref (cons ty1 tys) ix)]
-              [actual-ty (type-error `(Vector ...) actual-ty vec-exp expr)])]
-           [actual-ty (type-error `Integer actual-ty ix expr)])]
+           [(and ix^ `(has-type ,_ ,actual-ty))
+            (match actual-ty
+              [`Integer
+               (match ((typecheck env) vec-exp)
+                 [(and vec-exp^ `(has-type ,_ ,actual-ty))
+                  (match actual-ty
+                    [`(Vector ,ty1 ,tys ...)
+                     `(has-type (vector-ref ,vec-exp^ ,ix^) ,(list-ref (cons ty1 tys) ix))]
+                    [_ (type-error `(Vector ...) actual-ty vec-exp expr)])])]
+              [_ (type-error `Integer actual-ty ix expr)])])]
         [`(vector-set! ,vec-exp ,ix ,new-val)
          (match ((typecheck env) ix)
-           [`Integer
-            (match ((typecheck env) vec-exp)
-              [`(Vector ,ty1 ,tys ...)
-               (let ([new-val-ty ((typecheck env) new-val)]
-                     [old-val-ty (list-ref (cons ty1 tys) ix)])
-                 (if (equal? old-val-ty new-val-ty)
-                     `Void
-                     (type-error old-val-ty new-val-ty new-val expr)))]
-              [actual-ty (type-error `(Vector ...) actual-ty vec-exp expr)])]
-           [actual-ty (type-error `Integer actual-ty ix expr)])]
-        [`(define ,(list fun `[,arg1 : ,ty1] ...) : ,ty-ret ,body)
-         (let ([actual-ty-ret ((typecheck (append (map cons arg1 ty1) env)) body)])
+           [(and ix^ `(has-type ,_ ,actual-ty))
+            (match actual-ty
+              [`Integer
+               (match ((typecheck env) vec-exp)
+                 [(and vec-exp^ `(has-type ,_ ,actual-ty))
+                  (match actual-ty
+                    [`(Vector ,ty1 ,tys ...)
+                     (match-let ([(and new-val^ `(has-type ,_ ,new-val-ty))
+                                  ((typecheck env) new-val)]
+                                 [old-val-ty (list-ref (cons ty1 tys) ix)])
+                       (if (equal? old-val-ty new-val-ty)
+                           `(has-type (vector-set! ,vec-exp^ ,ix^ ,new-val^) Void)
+                           (type-error old-val-ty new-val-ty new-val expr)))]
+                    [_ (type-error `(Vector ...) actual-ty vec-exp expr)])])]
+              [_ (type-error `Integer actual-ty ix expr)])])]
+        [`(define ,(and args (list fun `[,arg1 : ,ty1] ...)) : ,ty-ret ,body)
+         (match-let ([(and body^ `(has-type ,_ ,actual-ty-ret))
+                      ((typecheck (append (map cons arg1 ty1) env)) body)])
            (if (equal? ty-ret actual-ty-ret)
-               ty-ret
+               `(define ,args : ,ty-ret ,body^)
                (type-error ty-ret actual-ty-ret body expr)))]
         [`(program ,defs ... ,body)
          (let* ([def-types (map extract-define-type defs)]
                 [dup-name  (check-duplicates def-types #:key car)])
            (if dup-name
                (error (format "Duplicate defines for `~a`" (car dup-name)))
-               (let ([_ (map (typecheck (append def-types env)) defs)]
-                     [ret-ty ((typecheck (append def-types env)) body)])
+               (match-let ([defs^ (map (typecheck (append def-types env)) defs)]
+                           [(and body^ `(has-type ,_ ,ret-ty))
+                            ((typecheck (append def-types env)) body)])
                  `(program (type ,ret-ty)
-                           ,@defs
-                           ,body))))]
+                           ,@defs^
+                           ,body^))))]
         [`(,exp-rator ,exp-rands ...)
-         (let ([ty-rator  ((typecheck env) exp-rator)]
-               [tys-rands (map (typecheck env) exp-rands)])
+         (match-let ([(and exp-rator^ `(has-type ,_ ,ty-rator))
+                      ((typecheck env) exp-rator)]
+                     [(and exp-rands^ (list `(has-type ,_ ,tys-rands) ...))
+                      (map (typecheck env) exp-rands)])
            (match ty-rator
              [(list tys-rator-args ... -> ty-rator-ret)
               (if (equal? tys-rator-args tys-rands)
-                  ty-rator-ret
+                  `(has-type (,exp-rator^ ,@exp-rands) ,ty-rator-ret)
                   (type-error-fun-args exp-rator
                                        `(,@tys-rator-args -> ,ty-rator-ret)
                                        tys-rands
@@ -121,19 +142,24 @@
     [(list ty1 ... -> ty-ret) (length ty1)]))
 
 (define tc-unary-expr
-  (λ (arg-ty res-ty e env expr)
+  (λ (op arg-ty res-ty e env expr)
     (match ((typecheck env) e)
-      [(== arg-ty) res-ty]
-      [actual-ty (type-error arg-ty actual-ty e expr)])))
+      [(and e^ `(has-type ,_ ,actual-ty))
+       (if (equal? actual-ty arg-ty)
+           `(has-type (,op ,e^) ,res-ty)
+           (type-error arg-ty actual-ty e expr))])))
 
 (define tc-binary-expr
-  (λ (arg-ty res-ty e1 e2 env expr)
+  (λ (op arg-ty res-ty e1 e2 env expr)
     (match ((typecheck env) e1)
-      [(== arg-ty)
-       (match ((typecheck env) e2)
-         [(== arg-ty) res-ty]
-         [actual-ty (type-error arg-ty actual-ty e2 expr)])]
-      [actual-ty (type-error arg-ty actual-ty e1 expr)])))
+      [(and e1^ `(has-type ,_ ,actual-ty))
+       (if (equal? actual-ty arg-ty)
+           (match ((typecheck env) e2)
+             [(and e2^ `(has-type ,_ ,actual-ty))
+              (if (equal? actual-ty arg-ty)
+                  `(has-type (,op ,e1^ ,e2^) ,res-ty)
+                  (type-error arg-ty actual-ty e2 expr))])
+           (type-error arg-ty actual-ty e1 expr))])))
 
 (define type-error
   (λ (expected-ty actual-ty subexpr expr)
