5f8a0789f3a109bc9a6beb79f39f74466b34786b
Caner Derici <canerderici@gmail.com>
2016-04-10 20:56:14 -0400

    yet another huge fix

--- assign-homes.rkt
+++ assign-homes.rkt
@@ -98,7 +98,7 @@
 ; Instruction -> Set Variable
 (define read-variables
   (match-lambda
-    [`(,(or `addq `subq) ,arg1 ,arg2)
+    [`(,(or `addq `subq `cmpq) ,arg1 ,arg2)
      (set-union (variable arg1)
                 (variable arg2))]
     [(or `(movq ,arg1 (offset ,arg2 ,n)) `(movq (offset ,arg1 ,n) ,arg2)) (set-union (variable arg1) (variable arg2))]
